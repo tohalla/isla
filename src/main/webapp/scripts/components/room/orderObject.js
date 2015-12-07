62e787ffb83dded7dca431b656e75f3e3eaaa5f0
Touko Hallasmaa <touko.hallasmaa@outlook.com>
2015-12-07 04:36:54 +0200

    update fixes objectSort filter to work with multiple parameters

--- src/main/webapp/scripts/components/room/orderObject.js
+++ src/main/webapp/scripts/components/room/orderObject.js
@@ -16,15 +16,24 @@
           param.attribute = typeof param.attribute !== 'undefined' ? param.attribute : 'id';
           param.reverse = typeof param.reverse !== 'undefined' ? param.reverse : false;
           if(param.type === 'time'){
-            order = new Date(a[param.attribute]) < new Date(b[param.attribute]) ? 1 : -1;
+            if(a[param.attribute] != b[param.attribute]){
+              order = new Date(a[param.attribute]) < new Date(b[param.attribute]) ? 1 : -1;
+            }
           }
           else if(param.type === 'array'){
-            order = a[param.attribute].length < b[param.attribute].length ? 1 : -1;
+            if(a[param.attribute].length != b[param.attribute].length){
+              order = a[param.attribute].length < b[param.attribute].length ? 1 : -1;
+            }
           }
           else{
-            order = a[param.attribute] < b[param.attribute] ? 1 : -1;
+            if(a[param.attribute] != b[param.attribute]){
+              order = a[param.attribute] < b[param.attribute] ? 1 : -1;
+            }
           }
-          return false;
+          if(typeof order !== 'undefined') {
+            order = param.reverse ? -order: order;
+            return false;
+          }
         });
         return order;
       });
