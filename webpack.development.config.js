3b3f8d9411f7edcd7ad6ba31ebcf982e6b298d2c
Touko Hallasmaa <touko.hallasmaa@outlook.com>
2016-03-28 21:24:39 +0300

    update webpack conf

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -33,8 +33,16 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        test: /\.(png|svg)$/,
         loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
       },
       {
         test: /\.json$/,
